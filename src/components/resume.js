2853c74b887c59391a6c2c06e2fc95794d753df1
Noel Quezada <noelquezada@gmail.com>
2019-11-19 18:53:33 -0800

    changes

---
+++ src/components/resume.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import Education from './education';
+import Experience from './experience';
+import Skills from './skills';
+
+
+class Resume extends Component {
+  render() {
+    return(
+      <div>
+        <Grid>
+          <Cell col={4}>
+            <div style={{textAlign: 'center'}}>
+              <img
+                src="nono.jpg"
+                alt="avatar"
+                style={{height: '300px'}}
+                style={{width: '300px'}}
+                 />
+            </div>
+
+            <h2 style={{paddingTop: '2em'}}>Noel Quezada</h2>
+            <h4 style={{color: 'grey'}}>Programmer</h4>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <h5>Location</h5>
+            <p>San Fransisco, CA</p>
+            <h5>Email</h5>
+            <p>noel23quezada@gmail.com</p>
+            <h5>Web</h5>
+            <p>mywebsite.com</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+          </Cell>
+          <Cell className="resume-right-col" col={8}>
+            <h2>Education</h2>
+            <Education
+              startYear={2002}
+              endYear={2006}
+              schoolName="My University"
+              schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+               />
+               <Education
+                 startYear={2007}
+                 endYear={2009}
+                 schoolName="My 2nd University"
+                 schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+                  />
+                <hr style={{borderTop: '3px solid #e22947'}} />
+              <h2>Experience</h2>
+            <Experience
+              startYear={2009}
+              endYear={2012}
+              jobName="First Job"
+              jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+              />
+              <Experience
+                startYear={2012}
+                endYear={2016}
+                jobName="Second Job"
+                jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+                />
+              <hr style={{borderTop: '3px solid #e22947'}} />
+              <h2>Skills</h2>
+              <Skills
+                skill="javascript"
+                progress={85}
+                />
+                <Skills
+                  skill="HTML/CSS"
+                  progress={90}
+                  />
+                  <Skills
+                    skill="NodeJS"
+                    progress={70}
+                    />
+                    <Skills
+                      skill="React"
+                      progress={55}
+                      />
+                      <Skills
+                      skill="MongoDB"
+                      progress={60}
+                      />
+                      <Skills
+                      skill="Python"
+                      progress={55}
+                      />
+                      
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+export default Resume;
